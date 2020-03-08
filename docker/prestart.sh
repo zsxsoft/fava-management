141b17d48526793eaa5baed8661c5c502ee5ee5e
zsx <git@zsxsoft.com>
2020-03-09 01:30:36 +0800

    修正小问题，避免未加--rm时挂掉

--- docker/prestart.sh
+++ docker/prestart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python manage.py migrate
-echo "from django.contrib.auth.models import User;User.objects.create_superuser(username='${USERNAME}', password='${PASSWORD}', email='${USERNAME}')" | python manage.py shell
+echo "from django.contrib.auth.models import User;User.objects.create_superuser(username='${USERNAME}', password='${PASSWORD}', email='${USERNAME}')" | python manage.py shell || true
 export PASSWORD=
